8c0c609322e73adeaed11520da58f8f6162350af
Meryem Charit <charitmeryem@gmail.com>
2021-02-02 15:05:58 +0100

    Add CustomerQuery for Donneur with ville and GrpSanguin orderby Compteur

--- src/main/java/fstt/bloodDonation/spring/mongo/api/repository/DonneurRepository.java
+++ src/main/java/fstt/bloodDonation/spring/mongo/api/repository/DonneurRepository.java
@@ -1,9 +1,15 @@
 package fstt.bloodDonation.spring.mongo.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import fstt.bloodDonation.spring.mongo.api.model.Donneur;
 
 public interface DonneurRepository extends MongoRepository<Donneur, Integer>{
+	
+	
+	public List<Donneur> findByVilleAndGroupeSanguinOrderByCompteur(String ville,String GrpSanguin);
 
+	
 }
--- src/main/java/fstt/bloodDonation/spring/mongo/api/resource/DonneurController.java
+++ src/main/java/fstt/bloodDonation/spring/mongo/api/resource/DonneurController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fstt.bloodDonation.spring.mongo.api.model.Donneur;
@@ -35,6 +36,14 @@ public class DonneurController {
 	public List<Donneur> getDonneurs() {
 		return repository.findAll();
 	}
+	
+	@GetMapping("/findAllDonneursForReceveur")
+	public List<Donneur> getDonneursForReceveur(@RequestParam(name="ville")String ville,@RequestParam(name = "grpSanguin")String groupeSanguin) {
+		
+		
+		return repository.findByVilleAndGroupeSanguinOrderByCompteur(ville, groupeSanguin);
+	}
+	
 
 	@GetMapping("/findAllDonneurs/{id}")
 	public Optional<Donneur> getDonneur(@PathVariable int id) {
